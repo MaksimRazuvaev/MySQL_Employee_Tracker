1b12bb4c54c6ff113b88b29df6f2d8da1fb22bd4
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2022-12-14 11:29:55 -0800

    create database and tables sql file created. fill up tables with data sql file is created. json file with npm dependences filled up

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (name)
+VALUES  (sales),
+        (hr),
+        (home);
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("scheff", 50000, 2),
+        ("sales person", 31000.00, 1),
+        ("hr", 31000.00, 2),
+        ("senior hr", 35000.00, 2),
+        ("janitor ", 25000.00, 3),
+        ("receptionist", 31000.00, 1),
+        ("CEO", 55000.00, 2),
+        ("accounter", 31000.00, 3),
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("name1", "second1" 7, NULL),
+        ("name2", "second2" 1, 1),
+        ("name3", "second3" 4, 1),
+        ("name4", "second4" 3, 3),
+        ("name5", "second5" 8, 1),
+        ("name6", "second6" 6, 1),
+        ("name7", "second7" 2, 2),
+        ("name8", "second8" 2, 2),
+        ("name9", "second9" 2, 2),
+        ("name10", "second10" 5, 3);
